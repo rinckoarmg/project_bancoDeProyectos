3a1b59b85bd1ce52c2d741ad55b5e3a6ced8816a
rinckarmg <rinckoar@gmail.com>
2021-11-18 10:57:27 -0500

    cambios project page y model project

--- lib/app/domain/models/project_model.dart
+++ lib/app/domain/models/project_model.dart
@@ -1,48 +1,47 @@
-// To parse required this JSON data, do
+// To parse this JSON data, do
 //
-//     final stores = storesFromMap(jsonString);
+//     final projects = projectsFromMap(jsonString);
 
 import 'dart:convert';
 
 class Projects {
   Projects({
-    required this.description,
-    required this.name,
-    required this.image,
+    required this.category,
     required this.contact,
+    required this.decription,
+    required this.image,
+    required this.name,
     this.web,
     this.id,
-    required this.category,
-
   });
 
-  String description;
-  String name;
-  String image;
+  String category;
   String contact;
+  String decription;
+  String image;
+  String name;
   String? web;
   String? id;
-  String category;
 
   factory Projects.fromJson(String str) => Projects.fromMap(json.decode(str));
 
   String toJson() => json.encode(toMap());
 
   factory Projects.fromMap(Map<String, dynamic> json) => Projects(
-        description: json["description"],
-        name: json["name"],
-        image: json["image"],
-        contact: json["contact"],
-        web: json["web"],
         category: json["category"],
+        contact: json["contact"],
+        decription: json["decription"],
+        image: json["image"],
+        name: json["name"],
+        web: json["web"],
       );
 
   Map<String, dynamic> toMap() => {
-        "description": description,
-        "name": name,
-        "image": image,
-        "contact": contact,
-        "web": web,
         "category": category,
+        "contact": contact,
+        "decription": decription,
+        "image": image,
+        "name": name,
+        "web": web,
       };
 }
