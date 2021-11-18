7d4bf61131305d161e2d9252f7738676487a03a1
tacoradam <tacora.dam@gmail.com>
2021-11-18 08:57:00 -0500

    project_card

--- lib/app/ui/pages/home/home_page.dart
+++ lib/app/ui/pages/home/home_page.dart
@@ -24,6 +24,7 @@ class HomePage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Icon(Icons.add),
+        backgroundColor: Colors.purple,
       ),
     );
   }
