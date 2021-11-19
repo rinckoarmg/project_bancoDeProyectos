1b39ca6054fb798114d0062519202f894d784ac0
tacoradam <tacora.dam@gmail.com>
2021-11-18 22:22:09 -0500

    update add_page

--- lib/app/ui/pages/add_project/add_project.dart
+++ lib/app/ui/pages/add_project/add_project.dart
@@ -9,7 +9,7 @@ class AddProjectPage extends StatefulWidget {
 }
 
 class _AddProjectPageState extends State<AddProjectPage> {
-  String _opcionSeleccionada = 'Fin de la pobreza';
+  String _opcionSeleccionada = 'Todas las Categorías';
   final List<String> _categories = [
     'Fin de la Pobreza',
     'Hambre Cero',
@@ -27,7 +27,8 @@ class _AddProjectPageState extends State<AddProjectPage> {
     'Vida Submarina',
     'Vida de Ecosistemas Terrestres',
     'Paz, Justicia e Instituciones Sólidas',
-    'Alianzas para lograr los objetivos'
+    'Alianzas para lograr los objetivos',
+    'Todas las Categorías'
   ];
   @override
   Widget build(BuildContext context) {
@@ -44,7 +45,6 @@ class _AddProjectPageState extends State<AddProjectPage> {
                 child: Container(
                     margin: EdgeInsets.symmetric(vertical: 10),
                     width: double.infinity,
-                    //height: size.height * 0.7,
                     decoration: BoxDecoration(
                         color: Colors.white,
                         borderRadius: BorderRadius.circular(25),
@@ -56,14 +56,15 @@ class _AddProjectPageState extends State<AddProjectPage> {
                           )
                         ]),
                     child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
+                        _categoria(),
                         _nombre(),
                         _descripcion(),
                         _telefono(),
                         _email(),
                         _web(),
-                        _categoria(),
                         // _pais(),
                         // _imagen(),
                         SizedBox(height: 30)
@@ -77,6 +78,48 @@ class _AddProjectPageState extends State<AddProjectPage> {
     );
   }
 
+
+  Widget _categoria() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(top: 20),
+          child: Text(
+            'Categoria:',
+              style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Monserrat',
+              fontSize: 20,
+              color: Colors.purple[800],
+            ),
+          ),
+        ),
+        DropdownButton(
+          value: _opcionSeleccionada,
+          items: getOpcionesDropdown(),
+          onChanged: (opt) {
+            print(opt);
+            setState(() {
+              _opcionSeleccionada = opt.toString();
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  List<DropdownMenuItem<String>> getOpcionesDropdown() {
+    List<DropdownMenuItem<String>> lista = [];
+    _categories.forEach((i) {
+      lista.add(DropdownMenuItem(
+        child: Text(i),
+        value: i,
+      ));
+    });
+    return lista;
+  }
+
   Widget _nombre() {
     final textStyle = TextStyle(
       fontWeight: FontWeight.bold,
@@ -85,18 +128,20 @@ class _AddProjectPageState extends State<AddProjectPage> {
       color: Colors.purple[800],
     );
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Padding(
           padding: EdgeInsets.only(top: 20, right: 25, left: 25),
           child: Text(
             'Nombre del proyecto',
-            textAlign: TextAlign.left,
             style: textStyle,
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 25),
-          child: TextField(),
+        Center(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25),
+            child: TextField(),
+          ),
         ),
       ],
     );
@@ -107,38 +152,6 @@ class _AddProjectPageState extends State<AddProjectPage> {
   // Widget _pais() {}
 
   // Widget _categoria() {}
-  List<DropdownMenuItem<String>> getOpcionesDropdown() {
-    List<DropdownMenuItem<String>> lista = [];
-    _categories.forEach((poder) {
-      lista.add(DropdownMenuItem(
-        child: Text(poder),
-        value: poder,
-      ));
-    });
-    return lista;
-  }
-
-  Widget _categoria() {
-    return Row(
-      children: [
-        Icon(Icons.select_all),
-        SizedBox(
-          width: 20,
-        ),
-        Expanded(
-          child: DropdownButton(
-            value: _opcionSeleccionada,
-            items: getOpcionesDropdown(),
-            onChanged: (opt) {
-              setState(() {
-                _opcionSeleccionada = opt.toString();
-              });
-            },
-          ),
-        )
-      ],
-    );
-  }
 
   Widget _web() {
     final textStyle = TextStyle(
@@ -235,10 +248,14 @@ class _AddProjectPageState extends State<AddProjectPage> {
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 25),
           child: TextField(
-            maxLines: 10,
+            maxLines: 15,
+            minLines: 1,
+            textAlign: TextAlign.justify,
+
           ),
         ),
       ],
     );
   }
+
 }
